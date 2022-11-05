2ed5e967c8436a673152514710119c35ad341e1f
Mirsario <me@mirsar.io>
2022-11-05 20:21:08 +0300

    Fixed & reviewed 'Terraria' after last commit.

--- patches/Terraria/Terraria/MonoLaunch.cs
+++ patches/Terraria/Terraria/MonoLaunch.cs
@@ -7,13 +7,14 @@ using Terraria;
 
 internal static class MonoLaunch
 {
-
 	private static readonly Dictionary<string, IntPtr> assemblies = new Dictionary<string, IntPtr>();
 
-	private static void Main(string[] args) {
+	private static void Main(string[] args)
+	{
 		AppDomain.CurrentDomain.AssemblyResolve += delegate (object sender, ResolveEventArgs sargs) {
 			string resourceName = new AssemblyName(sargs.Name).Name + ".dll";
 			string text = Array.Find(typeof(Program).Assembly.GetManifestResourceNames(), (string element) => element.EndsWith(resourceName));
+
 			if (text == null)
 				return null;
 
